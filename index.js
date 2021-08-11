0fc5c4fce69587b795c137f0ae763ee4fdc44f6a
Brittney Jackson <brnjackson@yahoo.com>
2021-08-10 21:03:53 -0400

    part two/extra credit completed

--- index.js
+++ index.js
@@ -8,8 +8,39 @@ app.get('/', (request, response) => {
   return response.send('Hello World')
 })
 
+app.use(express.json())
+
 app.get('/teams', (request, response) => {
   return response.send({ teams })
+})
+
+app.post('/teams', (request, response) => {
+  // eslint-disable-next-line no-console
+  console.log(request.body)
+  const {
+    location, mascot, abbreviation, conference, division
+  } = request.body
+
+  // check response body for all fields - if missing fields, return error 400
+  if (!location || !mascot || !abbreviation || !conference || !division) {
+    return response.status(400).send('missing fields')
+  }
+  // if all fields complete, get id of last team and add 1
+  const newTeamId = teams[teams.length - 1].id + 1
+  // if not, create a new team from the data provided
+  const newTeamObject = {
+    id: newTeamId,
+    location: location,
+    mascot: mascot,
+    abbreviation: abbreviation,
+    conference: conference,
+    division: division
+  }
+  // add new team object to the teams array
+
+  teams.push(newTeamObject)
+  // respond with the newly created team
+  response.send(newTeamObject)
 })
 
 // GET /teams/ID - Returns the team associated with that
